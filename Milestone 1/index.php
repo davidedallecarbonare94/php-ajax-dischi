e31d717f7c8f1b0ed7e9f2dc67a75b6911c08a31
davidedallecarbonare94 <davide.dallecarbonare94@gmail.com>
2021-06-24 18:44:02 +0200

    end project

--- Milestone 1/index.php
+++ Milestone 1/index.php
@@ -16,20 +16,22 @@ include __DIR__ . "\database.php";
 
 <body>
     <div id="root">
-        <header>
-            <h1>Spotify</h1>
-
+    <header>
+            <img src="./dist/img/Logo-della-Spotify.png" alt="">
         </header>
         <main>
-            <div class="main_container">
+            <div class="albums">
                 <?php 
                 foreach($discs as $disc){ ?>
 
                 <div class="card">
                     <img src="<?php echo $disc['poster']?>" alt="">
-                    <h1><?php echo $disc['title']?></h1>
-                    <span><?php echo $disc['author']?></span>
-                    <span><?php echo $disc['year']?></span>
+                    <div class="card_text">
+
+                        <h1><?php echo $disc['title']?></h1>
+                        <h2><?php echo $disc['author']?></h2>
+                        <h2><?php echo $disc['year']?></h2>
+                    </div>
                 </div>
 
                 <?php } ?>
